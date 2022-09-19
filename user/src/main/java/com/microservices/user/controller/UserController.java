94bb6156b06b1e3e1663d7e2b18349f267b2dcbe
Salih Yanbal <salih.yanbal@ideateknoloji.com>
2022-09-19 09:42:20 +0300

    api gateway created

--- user/src/main/java/com/microservices/user/controller/UserController.java
+++ user/src/main/java/com/microservices/user/controller/UserController.java
@@ -11,11 +11,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/user")
+@RequestMapping("")
 public class UserController {
 
     private final UserService userService;
-
 
     public UserController(UserService userService) {
         this.userService = userService;
